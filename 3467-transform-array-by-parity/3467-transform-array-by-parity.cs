b8a126c13782f47b1a3ad2a8892fc7003364f312
Daniel Zuzčák <93527124+firkraagg@users.noreply.github.com>
2026-01-19 22:30:54 +0100

    Time: 0 ms (100%), Space: 50 MB (27.42%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.cs
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.cs
@@ -1,10 +1,14 @@
 public class Solution {
     public int[] TransformArray(int[] nums) {
+        int even = 0;
         for (int i = 0; i < nums.Length; i++) {
-            nums[i] = nums[i] % 2 == 0 ? 0 : 1;
+            if (nums[i] % 2 == 0) even++;
         }
 
-        Array.Sort(nums);
+        for (int i = 0; i < nums.Length; i++) {
+            nums[i] = (i < even) ? 0 : 1;
+        }
+
         return nums;
     }
 }

f2b8677653902586bbae30797c1c0286ec64e075
Daniel Zuzčák <93527124+firkraagg@users.noreply.github.com>
2026-01-23 22:47:26 +0100

    Time: 0 ms (100%), Space: 42.7 MB (50.61%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cs
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cs
@@ -3,12 +3,12 @@ public class Solution {
     public int MaximumWealth(int[][] accounts) {
         int maxWealth = 0;
         for (int m = 0; m < accounts.Length; m++) {
-            int wealth = 0;
+            int actWealth = 0;
             for (int n = 0; n < accounts[m].Length; n++) {
-                wealth += accounts[m][n];
+                actWealth += accounts[m][n];
             }
 
-            maxWealth = wealth > maxWealth ? wealth : maxWealth;
+            maxWealth = actWealth > maxWealth ? actWealth : maxWealth;
         }
 
         return maxWealth;
